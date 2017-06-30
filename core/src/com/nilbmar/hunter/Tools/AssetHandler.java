c3d1063622f6e4e16a43b29911945b8c01223d9c
Nilbmar <Nilbmar@gmail.com>
2017-06-30 19:36:50 -0400

    Cleaned up AssetHandler

--- core/src/com/nilbmar/hunter/Tools/AssetHandler.java
+++ core/src/com/nilbmar/hunter/Tools/AssetHandler.java
@@ -15,10 +15,11 @@ public class AssetHandler {
     public final AssetManager manager = new AssetManager();
 
     // Location of texture packs for TextureAtlases
-    private String playerPack = "sprites/charFem.pack";
-    public final String enemyPack = "sprites/characters.pack";
-    public final String bulletPack = "sprites/ammo_and_effects.pack";
-    public final String itemPack = "sprites/pickups.pack";
+    private final String spritesLocation = "sprites/";
+    private String playerPack = spritesLocation + "charFem.pack";
+    private final String enemyPack = spritesLocation + "characters.pack";
+    private final String bulletPack = spritesLocation + "ammo_and_effects.pack";
+    private final String itemPack = spritesLocation + "pickups.pack";
 
     // Sprite Variables
     private TextureAtlas playerAtlas;
@@ -45,21 +46,10 @@ public class AssetHandler {
         itemAtlas = manager.get(itemPack);
     }
 
-    public TextureAtlas getPlayerAtlas() {
-        return playerAtlas;
-    }
-
-    public TextureAtlas getEnemyAtlas() {
-        return enemyAtlas;
-    }
-
-    public TextureAtlas getBulletAtlas() {
-        return bulletAtlas;
-    }
-
-    public TextureAtlas getItemAtlas() {
-        return itemAtlas;
-    }
+    public TextureAtlas getPlayerAtlas() { return playerAtlas; }
+    public TextureAtlas getEnemyAtlas() { return enemyAtlas; }
+    public TextureAtlas getBulletAtlas() { return bulletAtlas; }
+    public TextureAtlas getItemAtlas() { return itemAtlas; }
 
     public String getPlayerPack() { return playerPack; }
     public void setPlayerPack(String fileName) { playerPack = fileName; }
