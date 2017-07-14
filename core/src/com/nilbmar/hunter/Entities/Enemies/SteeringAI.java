56c88c25d51ce3c0efc166dc5ce72f5d480b7d45
Nilbmar <Nilbmar@gmail.com>
2017-07-14 18:09:38 -0400

    Started adding AI handlers using gdx-ai

---
+++ core/src/com/nilbmar/hunter/Entities/Enemies/SteeringAI.java
@@ -0,0 +1,122 @@
+package com.nilbmar.hunter.Entities.Enemies;
+
+import com.badlogic.gdx.ai.steer.Steerable;
+import com.badlogic.gdx.ai.utils.Location;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by sysgeek on 7/1/17.
+ *
+ * AI: SteeringAI
+ * Purpose: Have Entity move toward a point
+ * with a possible "safe" distance
+ * TODO: CURRENTLY IN THE WRONG PACKAGE, NEEDS TO MOVE OUT OF ENEMIES AND INTO AN AI PACKAGE
+ */
+
+public class SteeringAI implements Steerable<Vector2> {
+    @Override
+    public Vector2 getLinearVelocity() {
+        return null;
+    }
+
+    @Override
+    public float getAngularVelocity() {
+        return 0;
+    }
+
+    @Override
+    public float getBoundingRadius() {
+        return 0;
+    }
+
+    @Override
+    public boolean isTagged() {
+        return false;
+    }
+
+    @Override
+    public void setTagged(boolean tagged) {
+
+    }
+
+    @Override
+    public float getZeroLinearSpeedThreshold() {
+        return 0;
+    }
+
+    @Override
+    public void setZeroLinearSpeedThreshold(float value) {
+
+    }
+
+    @Override
+    public float getMaxLinearSpeed() {
+        return 0;
+    }
+
+    @Override
+    public void setMaxLinearSpeed(float maxLinearSpeed) {
+
+    }
+
+    @Override
+    public float getMaxLinearAcceleration() {
+        return 0;
+    }
+
+    @Override
+    public void setMaxLinearAcceleration(float maxLinearAcceleration) {
+
+    }
+
+    @Override
+    public float getMaxAngularSpeed() {
+        return 0;
+    }
+
+    @Override
+    public void setMaxAngularSpeed(float maxAngularSpeed) {
+
+    }
+
+    @Override
+    public float getMaxAngularAcceleration() {
+        return 0;
+    }
+
+    @Override
+    public void setMaxAngularAcceleration(float maxAngularAcceleration) {
+
+    }
+
+    @Override
+    public Vector2 getPosition() {
+        return null;
+    }
+
+    @Override
+    public float getOrientation() {
+        return 0;
+    }
+
+    @Override
+    public void setOrientation(float orientation) {
+
+    }
+
+    @Override
+    public float vectorToAngle(Vector2 vector) {
+        return 0;
+    }
+
+    @Override
+    public Vector2 angleToVector(Vector2 outVector, float angle) {
+        return null;
+    }
+
+    @Override
+    public Location<Vector2> newLocation() {
+        return null;
+    }
+
+}
